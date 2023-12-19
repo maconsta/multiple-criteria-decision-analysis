b0b72735795570aaccb807c2fd693472823ba8ba
Boyan <bobiboychev9@gmail.com>
2023-12-19 23:02:56 +0200

    Add files via upload

--- methods/ahp.py
+++ methods/ahp.py
@@ -10,20 +10,26 @@ class AHP:
     def calculate_weights(self):
         # Use the Pairwise object to calculate the eigenvector
         self.weights = self.pairwise_matrix.calculate_eigenvector()
+        print("Calculated Weights:", self.weights)  # Print the calculated weights
 
     def weigh(self):
         for col in range(self.decision_matrix.normalized_matrix.shape[1]):
             self.decision_matrix.normalized_matrix[:, col] *= self.weights[col]
+        print("Weighted Normalized Matrix:\n", self.decision_matrix.normalized_matrix)  # Print the weighted normalized matrix
 
     def calculate_scores(self):
         scores = np.sum(self.decision_matrix.normalized_matrix, axis=1)
+        print("Calculated Scores:", scores)  # Print the calculated scores
         return scores
 
     def calculate_ahp(self):
         self.decision_matrix.normalize_l2()
+        print("Normalized Matrix in calculate_ahp:\n", self.decision_matrix.normalized_matrix)
         self.calculate_weights()
         self.weigh()
+        print("Weighted Normalized Matrix in calculate_ahp:\n", self.decision_matrix.normalized_matrix)
         scores = self.calculate_scores()
+        print("Scores in calculate_ahp:", scores)
 
         result = []
         for i in range(len(scores)):
@@ -31,5 +37,7 @@ class AHP:
             result.append({"name": name, "score": scores[i]})
 
         result = sorted(result, key=lambda d: d['score'], reverse=True)
-
+        print("Final Result:", result)  # Print the final result
         return result
+
+
