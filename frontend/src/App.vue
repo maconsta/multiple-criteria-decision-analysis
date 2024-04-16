67ca43cf91e4bf9585f939b697a41d16672e7ae2
makonsta <martin.konstantinov0610@gmail.com>
2024-04-16 17:30:38 +0300

    some style changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,5 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&display=swap');
-  html{
-    font-family: "Montserrat", sans-serif;
-    scroll-behavior: smooth;
-  }
-
-body {
-  background-color: #fff;
-}
+ /* can't be empty */
 </style>
