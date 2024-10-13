98a5b17ddbd7d38a9cef3c86279c7e7b1c85f745
maconsta <martin.konstantinov0610@gmail.com>
2024-10-13 14:53:28 +0300

    switch to env file; remove setup

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
+from os import environ as env
 
-from backend.app.db.db_config import db_config
+#db config vars
+host = env.get("host")
+user = env.get("user")
+password = env.get("password")
+database = env.get("database")
+port = env.get("port")
 
-url = f"postgresql+psycopg2://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 engine = create_engine(url)
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
