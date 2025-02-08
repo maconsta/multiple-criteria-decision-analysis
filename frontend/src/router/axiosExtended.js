61d9bdacc5dabdb66248b4d257511481097750ff
maconsta <77923562+maconsta@users.noreply.github.com>
2025-02-08 23:34:15 +0200

    Update axiosExtended.js

--- frontend/src/router/axiosExtended.js
+++ frontend/src/router/axiosExtended.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 import router from "@/router/index.js";
 
-console.log(process.env);
+const csrfToken = localStorage.getItem("csrfToken");
+console.log('CSRF Token:', csrfToken);
+
 const axiosExtended = axios.create({
-  baseURL: process.env.VUE_APP_AXIOS_BASE_URL,
-  withCredentials: true,
-  headers: {
-    "X-CSRF-TOKEN": localStorage.getItem("csrfToken"),
-  },
+    baseURL: process.env.VUE_APP_AXIOS_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "X-CSRF-TOKEN": csrfToken ? csrfToken : "",
+    },
 });
 
 axiosExtended.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status === 401) {
-      router.push({
-        name: "signIn",
-      });
+    (response) => response,
+    (error) => {
+        if (error.response.status === 401) {
+            router.push({
+                name: "signIn",
+            });
+        }
     }
-  }
 );
 
 export default axiosExtended;
